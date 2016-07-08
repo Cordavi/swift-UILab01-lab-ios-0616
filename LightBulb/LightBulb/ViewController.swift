cd94842df3fef8ca5ed80eecac3418bba430f209
Cordavi <michaelamundsen@gmail.com>
2016-07-08 14:04:46 -0400

    Added UISegmentedControl to allow you to select a color. Added functions that allow you to change the lightbulbs color based on selected color.

--- LightBulb/LightBulb/ViewController.swift
+++ LightBulb/LightBulb/ViewController.swift
@@ -13,14 +13,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // TODO: Change background color of lightBulb view to red
+        lightBulb.backgroundColor = "red".color
     }
 
     func changeColor(to color: UIColor) {
-        // TODO: Change background color to "color"
+        lightBulb.backgroundColor = color
     }
 
+
     @IBAction func colorSelected(sender: UISegmentedControl) {
-        // TODO: Change background color when segmented control changes
+        switch sender.selectedSegmentIndex {
+        case 0:
+            changeColor("red".color)
+        case 1:
+            changeColor("yellow".color)
+        case 2:
+            changeColor("blue".color)
+        case 3:
+            changeColor("green".color)
+        default:
+            break
+        }
     }
 }
